53a0271d95dcded0edf87bb17b62d5d3e9d34555
Hannes Riebl <hriebl@uni-goettingen.de>
2024-06-27 21:32:15 +0200

    Force TFP modules to load, fix model init (#5)
    * Force TFP modules to load, fix model init
    
    * Remove dummy module access, fail early instead
    
    * Fix if condition
    
    * Use Liesel from PyPI for tmp venv
    
    * Update Python for GitHub CI
    
    * Run GitHub CI on macOS
    
    * Don't install venv on GitHub CI
    
    * Don't install NumPy through virtualenv_create()
    
    * Try again on original GitHub CI config
    
    * Try without explicit venv installation on GitHub CI
    
    * Also run GitHub CI on macOS
    
    * Fix GitHub CI config

--- R/reticulate.R
+++ R/reticulate.R
@@ -76,9 +76,8 @@ NULL
 use_tmp_liesel_venv <- function(...) {
   path <- tempdir()
 
-  virtualenv_create(path, ...)
-  virtualenv_install(path, "git+https://github.com/liesel-devs/liesel.git",
-                     ignore_installed = TRUE)
+  virtualenv_create(path, packages = NULL, ...)
+  virtualenv_install(path, "liesel", ignore_installed = TRUE)
   try(virtualenv_install(path, "pygraphviz", ignore_installed = TRUE))
   use_virtualenv(path, required = TRUE)
 
